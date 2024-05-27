98ef6c14ab85be5d64dd49891ebf78a7ad3da07e
BlueBlizzardd <105523256+BlueBlizzardd@users.noreply.github.com>
2024-05-27 00:05:07 -0400

    FINALLY all routes fixed

--- src/Footer/Socials/Socials.tsx
+++ src/Footer/Socials/Socials.tsx
@@ -4,16 +4,16 @@ export default function Socials() {
     return (
         <div className={`${socials.container} flex`}>
             <div className={`${socials.icon} ${socials.fb} flex`}>
-                <img src="./src/assets/socials/facebook_white.svg" alt="facebook" />
+                <img src="/socials/facebook_white.svg" alt="facebook" />
             </div>
             <div className={`${socials.icon} ${socials.ig} flex`}>
-                <img src="./src/assets/socials/instagram_white.svg" alt="instagram" />
+                <img src="/socials/instagram_white.svg" alt="instagram" />
             </div>
             <div className={`${socials.icon} ${socials.twt} flex`}>
-                <img src="./src/assets/socials/twitter_white.svg" alt="twitter" />
+                <img src="/socials/twitter_white.svg" alt="twitter" />
             </div>
             <div className={`${socials.icon} ${socials.disc} flex`}>
-                <img src="./src/assets/socials/discord_white.svg" alt="discord" />
+                <img src="/socials/discord_white.svg" alt="discord" />
             </div>
         </div>
     )
